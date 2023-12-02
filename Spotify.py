1366d9a4c687661075955a3aa502db7930ed9acf
Chatterjee, Aniruddho Swapan <caswapan@iu.edu>
2023-12-02 12:14:49 -0500

    Initial Commit

--- Spotify.py
+++ Spotify.py
@@ -22,6 +22,6 @@ st.title('Top 5 Artists Every Year Based on Popularity')
 selected_year = st.sidebar.selectbox('Select Year', df['Year'].unique())
 yeardf = df[df['Year'] == selected_year]
 #artist_popularity = yeardf.groupby('Artist')['Popularity'].mean().sort_values(ascending=False)
-artist_popularity = yeardf.groupby(['Artist']).agg({'Popularity': 'mean'}).sort_values(by='Popularity',ascending=False)
-barfig = px.bar(artist_popularity, x='Artist', y='Popularity', title=f'Top 5 Artists in {selected_year}', labels={'Popularity': 'Average Popularity'})
+artist_popularity = yeardf.groupby(['Artist']).agg({'Popularity': 'mean'}).sort_values(by='Popularity',ascending=False).reset_index()
+barfig = px.bar(artist_popularity, x=artist_popularity['Artist'], y=artist_popularity['Popularity'], title=f'Top 5 Artists in {selected_year}', labels={'Popularity': 'Average Popularity'})
 st.plotly_chart(barfig)
